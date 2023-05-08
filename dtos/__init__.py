01ea75cc1e8bad71fc2c0bc353056ee7a9a19607
barisemremise <barisemremise@hotmail.com>
2023-05-09 00:23:18 +0300

    delete init.py

--- dtos/__init__.py
+++
@@ -1,4 +0,0 @@
-from .course_dto import *
-from .major_dto import *
-from .schedule_dto import *
-from .student_dto import *
--- enums/__init__.py
+++
@@ -1,7 +0,0 @@
-from .course_tags import *
-from .days import *
-from .grades import *
-from .languages import *
-from .semesters import *
-from .student_types import *
-from .teaching_methods import *
