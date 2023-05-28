50c6f65ee27d5118865f1e7908c8ba4e510c8b61
OmerFarukOzkan <59647178+OmerFarukOzkan@users.noreply.github.com>
2023-05-28 15:25:31 +0300

    Update schedule_dto.py

--- dtos/schedule_dto.py
+++ dtos/schedule_dto.py
@@ -1,5 +1,6 @@
 from typing import List, Any
 from dtos.course_dto import OpenedCourseSearchDto
+from datetime import Date
 
 class ScheduleDto:
     name: str
@@ -9,3 +10,4 @@ class ScheduleDto:
     future_plan: List[Any] = None
     preferences: List[str] = None
     student_id: str = None
+    time: Date = None    
